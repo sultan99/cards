863e62676fc82c5f67be8d3d3bdb3447fa9e13af
sultan99 <sultan99@mail.ru>
2024-03-17 20:55:26 -0400

    Refactor index.tsx to remove unused code and update MAX_RECORDS value

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,15 @@
 import PostList from './post-list'
-import React, {useEffect} from 'react'
+import React from 'react'
 import TopBar from './top-bar'
 import {Layout} from './styles.scss'
 import {useApp, usePosts} from '@/store'
 
-const MAX_RECORDS = 1000
+const MAX_RECORDS = 100
 
 const App = () => {
   const theme = useApp(`theme`)
   const fetchPosts = usePosts(`fetchPosts`)
   fetchPosts(MAX_RECORDS)
-  useEffect(() => {
-    setTimeout(() => useApp.set(`theme`, `dark`), 2000)
-  }, [])
 
   return (
     <Layout theme={theme}>
